ba099830b8ae023a424505dc542868f4461e7fae
Alban Linard <alban@linard.fr>
2017-02-21 15:58:00 +0000

    ===== model@ardoises/test-ardoise:master =====return function (Layer, layer, ref)  layer.vertices = {    b = {      position = {        x = 233.45909612981,        y = 371.90758472858,      },    },  }end

--- src/model.lua
+++ src/model.lua
@@ -26,5 +26,11 @@ return function (Layer, layer, ref)
         y = 118.42616554591,
       },
     },
+    b = {
+      position = {
+        x = 233.45909612981,
+        y = 371.90758472858,
+      },
+    },
   }
 end
