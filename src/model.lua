49a694ace082ac42e403265ef3095c2c5714701e
Alban Linard <alban@linard.fr>
2017-02-21 12:09:56 +0100

    Trying to put all rendering in the model.

--- src/model.lua
+++ src/model.lua
@@ -110,12 +110,133 @@ return function (Layer, example, ref)
 
   graph [meta] [gui] = {}
 
-  graph [meta] [gui].create = function (Adapter, proxy)
+  graph [meta] [gui].render = function (parameters)
+    assert (type (parameters) == "table")
+    local Adapter  = require "ardoises.js"
+    local Et       = require "etlua"
+    local layer    = assert (parameters.what  ).layer
+    local target   = assert (parameters.target)
+    local D3       = Adapter.window.d3
+    local width    = 960
+    local height   = 500
+    local hidden   = {}
+    local vertices = Adapter.js.new (Adapter.window.Array)
+    local edges    = Adapter.js.new (Adapter.window.Array)
+    for key, vertex in pairs (layer.vertices) do
+      local data = Adapter.tojs {
+        id = vertices.length,
+        x  = width  / 2,
+        y  = height / 2,
+      }
+      hidden [data] = {
+        id    = vertices.length,
+        key   = key,
+        proxy = vertex,
+      }
+      vertices [vertices.length] = data
+    end
+    for key, edge in pairs (layer.edges) do
+      local data = Adapter.tojs {
+        id = vertices.length,
+        x  = width  / 2,
+        y  = height / 2,
+      }
+      hidden [data] = {
+        id    = vertices.length,
+        key   = key,
+        proxy = edge,
+      }
+      vertices [vertices.length] = data
+      for k, arrow in pairs (edge.arrows) do
+        for i = 0, vertices.length-1 do
+          local node = vertices [i]
+          if arrow.vertex <= hidden [node].proxy then
+            local link = Adapter.js.new (Adapter.window.Object)
+            link.source = data
+            link.target = node
+            hidden [link] = {
+              id    = edges.length,
+              key   = k,
+              proxy = arrow,
+            }
+            edges [edges.length] = link
+          end
+        end
+      end
+    end
+    target.innerHTML = Et.render ([[
+      <svg width="960" height="500" id="layer">
+      </svg>
+    ]], {
+      width  = width,
+      height = height,
+    })
+    local svg = D3:select "#layer"
+    local g   = svg
+      :append "g"
+      :attr   ("class", ".ardoises-gui")
+    local simulation = D3
+      :forceSimulation ()
+      :force ("link"  , D3:forceLink ():id (function (_, d) return d.id end))
+      :force ("charge", D3:forceManyBody ())
+      :force ("center", D3:forceCenter (width / 2, height / 2))
+    local drag_start = function (_, vertex)
+      simulation:alphaTarget (1):restart ()
+      vertex.fx = vertex.x
+      vertex.fy = vertex.y
+    end
+    local drag_drag = function (_, vertex)
+      vertex.fx = D3.event.x
+      vertex.fy = D3.event.y
+    end
+    local links = g
+      :selectAll ".ardoises-gui"
+      :data      (edges)
+      :enter     ()
+      :append    "line"
+      :attr      ("stroke", "white")
+      :attr      ("stroke-width", 2)
+    local nodes = g
+      :selectAll ".ardoises-gui"
+      :data   (vertices)
+      :enter  ()
+      :append (function (_, data)
+         local proxy = hidden [data].proxy
+         return proxy [meta] [gui].create (Adapter, {
+           proxy = hidden [data],
+           data  = data,
+         })
+       end)
+      :call (D3:drag ():on ("start", drag_start):on ("drag" , drag_drag))
+    local source_x = function (_, d) return d.source.x end
+    local source_y = function (_, d) return d.source.y end
+    local target_x = function (_, d) return d.target.x end
+    local target_y = function (_, d) return d.target.y end
+    local tick     = function ()
+      links:attr ("x1", source_x)
+           :attr ("y1", source_y)
+           :attr ("x2", target_x)
+           :attr ("y2", target_y)
+      nodes:each (function (element, data)
+        local proxy = hidden [data].proxy
+        return proxy [meta] [gui].update (Adapter, {
+          element = element,
+          proxy   = hidden [data],
+          data    = data,
+        })
+      end)
+    end
+    simulation:nodes (vertices):on ("tick", tick)
+    simulation:force "link":links (edges)
+    svg:call (D3:zoom ():on ("zoom", function ()
+      g:attr ("transform", D3.event.transform)
+    end))
   end
 
   graph [meta].vertex_type [meta] [gui] = {}
 
-  graph [meta].vertex_type [meta] [gui].create = function (Adapter, t)
+  graph [meta].vertex_type [meta] [gui].create = function (parameters)
+    local Adapter   = require "ardoises.js"
     local group     = Adapter.document:createElementNS (Adapter.window.d3.namespaces.svg, "g")
     local selection = Adapter.window.d3:select (group)
     selection
@@ -126,17 +247,19 @@ return function (Layer, example, ref)
     return selection:node ()
   end
 
-  graph [meta].vertex_type [meta] [gui].update = function (Adapter, t)
+  graph [meta].vertex_type [meta] [gui].update = function (parameters)
+    local Adapter = require "ardoises.js"
     Adapter.window.d3
-      :select (t.element)
+      :select (parameters.element)
       :selectAll "circle"
-      :attr ("cx", t.data.x)
-      :attr ("cy", t.data.y)
+      :attr ("cx", parameters.data.x)
+      :attr ("cy", parameters.data.y)
   end
 
   graph [meta].edge_type [meta] [gui] = {}
 
-  graph [meta].edge_type [meta] [gui].create = function (Adapter, t)
+  graph [meta].edge_type [meta] [gui].create = function (parameters)
+    local Adapter   = require "ardoises.js"
     local group     = Adapter.document:createElementNS (Adapter.window.d3.namespaces.svg, "g")
     local selection = Adapter.window.d3:select (group)
     selection
@@ -146,12 +269,13 @@ return function (Layer, example, ref)
     return selection:node ()
   end
 
-  graph [meta].edge_type [meta] [gui].update = function (Adapter, t)
+  graph [meta].edge_type [meta] [gui].update = function (parameters)
+    local Adapter = require "ardoises.js"
     Adapter.window.d3
-      :select (t.element)
+      :select (parameters.element)
       :selectAll "circle"
-      :attr ("cx", t.data.x)
-      :attr ("cy", t.data.y)
+      :attr ("cx", parameters.data.x)
+      :attr ("cy", parameters.data.y)
   end
 
   local binary_edges = Layer.new { name = "graph.binary_edges" }
